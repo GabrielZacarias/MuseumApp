ac356d901fbf6690de157317b0b2b82a4f51bebe
GabrielZacarias <gabezacarias@outlook.com>
2020-12-02 22:52:56 -0500

    magicnumbers file added

---
+++ app/src/main/java/com/museumtickets/MagicNumbers.java
@@ -0,0 +1,20 @@
+package com.museumtickets;
+
+public class MagicNumbers {
+    public static final int MOMA_ADULT = 25;
+    public static final int MOMA_SENIOR = 17;
+    public static final int MOMA_STUDENT = 12;
+
+    public static final int MMA_ADULT = 25;
+    public static final int MMA_SENIOR = 18;
+    public static final int MMA_STUDENT = 14;
+
+    public static final int AMN_ADULT = 23;
+    public static final int AMN_SENIOR = 18;
+    public static final int AMN_STUDENT = 18;
+
+    public static final int SGM_ADULT = 25;
+    public static final int SGM_SENIOR = 18;
+    public static final int SGM_STUDENT = 18;
+
+}
